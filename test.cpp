5058a85ad8b6914e282269b5e0d8582ffa20fd6a
VoktexYT <ubguertin@gmail.com>
2024-10-14 18:53:27 -0400

    test

--- test.cpp
+++ test.cpp
@@ -1,16 +1,10 @@
 #include <gtest/gtest.h>
 
-int add(int a, int b) {
-    return a + b;
-}
+int add(int a, int b) { return a + b + 1; }
 
-TEST(AdditionTest, HandlesPositiveInput) {
-    EXPECT_EQ(add(1, 2), 3);
-}
+TEST(AdditionTest, HandlesPositiveInput) { EXPECT_EQ(add(1, 2), 3); }
 
-TEST(AdditionTest, HandlesNegativeInput) {
-    EXPECT_EQ(add(-1, -2), -3);
-}
+TEST(AdditionTest, HandlesNegativeInput) { EXPECT_EQ(add(-1, -2), -3); }
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
